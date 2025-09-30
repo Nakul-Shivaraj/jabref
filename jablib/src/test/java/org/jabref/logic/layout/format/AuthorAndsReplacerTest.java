9bbe6e881029f84e11c04f4c05e169fda46f0e85
Nakul Shivaraj <nakul.s142@gmail.com>
2025-09-29 19:57:51 -0700

    Test: Refactor assigned test classes to use ParmeterizedTest

--- jablib/src/test/java/org/jabref/logic/layout/format/AuthorAndsReplacerTest.java
+++ jablib/src/test/java/org/jabref/logic/layout/format/AuthorAndsReplacerTest.java
@@ -10,15 +10,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AuthorAndsReplacerTest {
 
     /**
-     * Test method for {@link org.jabref.logic.layout.format.AuthorAndsReplacer#format(java.lang.String)}.
+     * Test method for
+     * {@link org.jabref.logic.layout.format.AuthorAndsReplacer#format(java.lang.String)}.
      */
     @ParameterizedTest
     @CsvSource({
+            // Empty case
             "'', ''",
+
+            // Single name (unchanged)
             "'Someone, Van Something', 'Someone, Van Something'",
+
+            // 'and' replaced with '&' in two names
             "'John Smith and Black Brown, Peter', 'John Smith & Black Brown, Peter'",
-            "'von Neumann, John and Smith, John and Black Brown, Peter', 'von Neumann, John; Smith, John & Black Brown, Peter'",
-            "'John von Neumann and John Smith and Peter Black Brown', 'John von Neumann; John Smith & Peter Black Brown'"
+
+            // 'and' replaced with ';' and '&' for three names
+            "'von Neumann, John and Smith, John and Black Brown, Peter', "
+                    + "'von Neumann, John; Smith, John & Black Brown, Peter'",
+
+            // 'and' replaced with ';' and '&' for three names
+            "'John von Neumann and John Smith and Peter Black Brown', "
+                    + "'John von Neumann; John Smith & Peter Black Brown'"
     })
     void format(String input, String expected) {
         LayoutFormatter a = new AuthorAndsReplacer();
